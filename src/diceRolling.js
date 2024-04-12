46abbb7b79fef7d4f0f84c26858bca5eda6e9fd3
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-04-13 09:51:49 +1000

    feat: added comments

--- src/diceRolling.js
+++ src/diceRolling.js
@@ -1,10 +1,24 @@
 
 
+/**
+ * Rolls a dice of diceSize and returns the result
+ *
+ * @param {number} [diceSize=6]
+ * @returns {*}
+ */
+
 function rollDice(diceSize = 6) {
     let rollResult = Math.floor(Math.random() * diceSize) + 1;
     return rollResult
 }
 
+
+/**
+ * Rolls a dice of diceSize and returns the higher of the two results
+ * Also outputs the two numbers rolled
+ * @param {number} [diceSize=6]
+ * @returns {{ finalResult: any; rolls: {}; }}
+ */
 function rollDiceWithAdvantage(diceSize = 6){
     let result = {
         finalResult: null,
@@ -17,6 +31,12 @@ function rollDiceWithAdvantage(diceSize = 6){
     result.finalResult = Math.max(...result.rolls)
     return result;
 }
+/**
+ * Rolls a dice of diceSize and returns the lower of the two results
+ * Also outputs the two numbers rolled
+ * @param {number} [diceSize=6]
+ * @returns {{ finalResult: any; rolls: {}; }}
+ */
 function rollDiceWithDisadvantage(diceSize = 6){
     let result = {
         finalResult: null,
