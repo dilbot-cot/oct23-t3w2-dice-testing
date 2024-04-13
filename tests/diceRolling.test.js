a03e73beeb09577d432ca4d80b31eeecc0fd1129
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-04-13 10:25:48 +1000

    feat: added jest comments

--- tests/diceRolling.test.js
+++ tests/diceRolling.test.js
@@ -1,11 +1,28 @@
 const { rollDice } = require("../src/diceRolling")
 
+beforeEach(() => {
+    // before each individual test runs
+    console.log('A test has been started');
+});
+
+afterEach(() => {
+    console.log('A test has finished');
+});
+
+beforeAll(() => {
+    console.log('Testing will begin');
+});
+
+afterAll(() => {
+    console.log('All tests completed');
+});
+
 describe('As a casual game ...', () => {
   test('... I want to roll a 6-sided dice...', () => {
     let result = rollDice();
 
-    expect(result).toBeGreaterThan(0);
     expect(result).toBeLessThanOrEqual(6);
-  })
-  
+    expect(result).toBeGreaterThanOrEqual(1);
+  });
+
 });
